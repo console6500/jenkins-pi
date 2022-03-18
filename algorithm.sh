3fb3d13f79dccd4f306d42af86d7747ccdbd5d10
console6500 <73613222+console6500@users.noreply.github.com>
2022-03-18 16:13:21 -0700

    Update algorithm.sh

--- algorithm.sh
+++ algorithm.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
+echo "3.14159" | tee report.txt
+
+# UNCOMMENT THE FOLLOWING LINE TO GET A MORE PRECISE CALCULATION OF PI.
+# ALSO REMOVE THE PREVIOUS ECHO STATEMENT.
+# echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
